6a5a6bb9378c763e029b830d67b96fdb1c5649bb
Preetham Sridhar <preetham.prem2325@gmail.com>
2020-03-02 13:46:52 +0530

    add readme

---
+++ src/container/registration/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Card, Form} from 'antd';
+import './styles.css';
+
+const Index = () => {
+  return (
+    <div className="reg-div">
+      <Card className="reg-card">
+        <h2>Registration form</h2>
+      </Card>
+    </div>
+  )
+}
+
+export default Index
